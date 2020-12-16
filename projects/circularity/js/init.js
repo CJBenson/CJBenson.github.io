60d928f72a0a544701b9ebdfb46870eff84ac542
CJBenson <cjben2002@gmail.com>
2020-12-16 22:49:17 +0000

    adding projects

---
+++ projects/circularity/js/init.js
@@ -0,0 +1,99 @@
+var init = function (window) {
+    'use strict';
+    var 
+        draw = window.opspark.draw,
+        physikz = window.opspark.racket.physikz,
+        
+        app = window.opspark.makeApp(),
+        canvas = app.canvas, 
+        view = app.view,
+        fps = draw.fps('#000'),
+        
+        drawCircle,
+        circles;
+        
+    
+    window.opspark.makeGame = function() {
+        
+        window.opspark.game = {};
+        var game = window.opspark.game;
+        
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM SETUP ////////////////////////////
+        ////////////////////////////////////////////////////////////
+        var circle;
+        var circles = [];
+
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
+
+        for (var i = 0; i < 100; i++) {
+            drawCircle();
+        }
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM LOGIC ////////////////////////////
+        ////////////////////////////////////////////////////////////
+        
+        /* 
+        This Function is called 60 times/second producing 60 frames/second.
+        In each frame, for every circle, it should redraw that circle
+        and check to see if it has drifted off the screen.         
+        */
+        function update() {
+            for (var i = 0; i < circles.length; i++) {
+                var circleNum = circles[i];
+                physikz.updatePosition(circleNum);
+                game.checkCirclePosition(circleNum);
+            }
+        }
+    
+        /* 
+        This Function should check the position of a circle that is passed to the 
+        Function. If that circle drifts off the screen, this Function should move
+        it to the opposite side of the screen.
+        */
+        var rightEdge = circle.x + circle.radius;
+        var leftEdge = circle.x - circle.radius;
+        var topEdge = circle.y - circle.radius;
+        var bottomEdge = circle.y + circle.radius;
+        game.checkCirclePosition = function(circle) {
+
+            // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
+            if ( leftEdge > canvas.width ) {
+                circle.x = 0;
+            } else if( rightEdge < 0 ) {
+                circle.x = canvas.width;
+            } else if( bottomEdge > canvas.height ) {
+                circle.y = 0;
+            } else if( topEdge < 0 ) {
+                circle.y = canvas.height;
+            }
+            
+        }
+        
+        /////////////////////////////////////////////////////////////
+        // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
+        /////////////////////////////////////////////////////////////
+        
+        view.addChild(fps);
+        app.addUpdateable(fps);
+        
+        game.circle = circle;
+        game.circles = circles;
+        game.drawCircle = drawCircle;
+        game.update = update;
+        
+        app.addUpdateable(window.opspark.game);
+    }
+};
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports = init;
+}
